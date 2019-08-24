e86a7661a4d593d4f089d7d3cc276ab5d91222ea
who <hoellinger@computer.org>
2019-08-24 17:19:30 +0200

    Draw lines on left button click

--- PyDrawCanvas.py
+++ PyDrawCanvas.py
@@ -5,13 +5,23 @@ class PyDrawCanvas(tk.Canvas):
 
     def __init__(self, master=None, **kwargs):
         super().__init__(master=master, **kwargs)
+        self.points=[]
         self.initEvents()
 
     def initEvents(self):
         self.bind("<Configure>", self.on_resize)
+        self.bind("<ButtonPress-1>", self.on_left_click)
 
     def on_resize(self, event):
         self.redraw()
+
+    def on_left_click(self, event):
+        x, y = event.x, event.y
+        self.add_point(x,y)
+        self.redraw()
+
+    def add_point(self, x,y):
+        self.points.append((x,y))
 
     def redraw(self):
         self.delete("all")
@@ -19,3 +29,7 @@ class PyDrawCanvas(tk.Canvas):
         h, w = self.winfo_height(), self.winfo_width()
         text = "Hello PyDraw"
         self.create_text(w / 2, h / 2, text=text, anchor="center")
+
+        if len(self.points) >= 2:
+            flat_xy = [n for point in self.points for n in point]
+            self.create_line(flat_xy)

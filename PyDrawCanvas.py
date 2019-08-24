423c6cd01aa7306204995ac1bb8b15d1e67d3dbb
who <hoellinger@computer.org>
2019-08-24 17:24:02 +0200

    use activefill to display focus of object

--- PyDrawCanvas.py
+++ PyDrawCanvas.py
@@ -5,7 +5,7 @@ class PyDrawCanvas(tk.Canvas):
 
     def __init__(self, master=None, **kwargs):
         super().__init__(master=master, **kwargs)
-        self.points=[]
+        self.points = []
         self.initEvents()
 
     def initEvents(self):
@@ -17,19 +17,19 @@ class PyDrawCanvas(tk.Canvas):
 
     def on_left_click(self, event):
         x, y = event.x, event.y
-        self.add_point(x,y)
+        self.add_point(x, y)
         self.redraw()
 
-    def add_point(self, x,y):
-        self.points.append((x,y))
+    def add_point(self, x, y):
+        self.points.append((x, y))
 
     def redraw(self):
         self.delete("all")
 
         h, w = self.winfo_height(), self.winfo_width()
         text = "Hello PyDraw"
-        self.create_text(w / 2, h / 2, text=text, anchor="center")
+        self.create_text(w / 2, h / 2, text=text, anchor="center", activefill="red")
 
         if len(self.points) >= 2:
             flat_xy = [n for point in self.points for n in point]
-            self.create_line(flat_xy)
+            self.create_line(flat_xy, activefill="red")
